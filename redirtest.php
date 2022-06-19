b9c7886c319e988ffa4a8887af3255134e42702b
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2022-06-19 05:18:47 +0000

    bitrix repo created

---
+++ redirtest.php
@@ -0,0 +1,6 @@
+<? 
+require ( $_SERVER [ "DOCUMENT_ROOT" ]. "/bitrix/header.php" );
+LocalRedirect( "http://lanacode.ru/" );
+echo   "точно не работает!"; 
+require ( $_SERVER [ "DOCUMENT_ROOT" ]. "/bitrix/footer.php" );
+?> 
